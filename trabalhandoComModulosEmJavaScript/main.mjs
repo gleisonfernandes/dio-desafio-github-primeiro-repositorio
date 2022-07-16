d6deec9ee872e2be17a528ec002dd770ccadc198
gleisonfernandes <gleison.fernandesb@gmail.com>
2022-07-16 12:25:11 -0300

    Adicionando a atividade da aula Trabalhando com Módulos em JavaScript

---
+++ trabalhandoComModulosEmJavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Gleison', 32));
+console.log(mostraCidade('Gleison', 'Alagoinhas-BA'));
+console.log(mostraHobby('Glison', 'Jogar Futebol'));
